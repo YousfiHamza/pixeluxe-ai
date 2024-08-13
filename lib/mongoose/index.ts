bbe29f8fb90fc0476ae34dd8f74803c404716342
Hamza <yousfihamza22@gmail.com>
2024-08-13 13:19:50 +0200

    chore: MongoDB initial setups

---
+++ lib/mongoose/index.ts
@@ -0,0 +1,34 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+interface MongooseConnection {
+  connection: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    connection: null,
+    promise: null,
+  };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.connection) return cached.connection;
+
+  if (!MONGODB_URI) throw new Error("Missing MONGODB_URI");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "Imaginify",
+      bufferCommands: false,
+    });
+
+  cached.connection = await cached.promise;
+
+  return cached.connection;
+};
